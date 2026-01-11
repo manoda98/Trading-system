434af54362889698898157f70418863fc037a3ff
manoda98 <manodasene@gmail.com>
2026-01-11 13:10:38 +0530

    initialize kafka producer and consumer on server startup

--- order-manager/app.js
+++ order-manager/app.js
@@ -5,7 +5,8 @@ const authenticationRoutes = require('./routes/authenticationRoutes');
 const instrumentRoutes = require('./routes/instrumentsRoutes');
 const accountsRoutes = require('./routes/accountsRoutes');
 const usersRoutes = require('./routes/usersRoutes');
-const cors = require('cors')
+const cors = require('cors');
+const { initialize } = require('./kafka/client')
 
 const app = express();
 app.use(cors())
@@ -36,4 +37,7 @@ app.use('/api/users', usersRoutes);
 
  
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, async () => {
+    console.log(`Server running on port ${PORT}`);
+    await initialize();
+} );

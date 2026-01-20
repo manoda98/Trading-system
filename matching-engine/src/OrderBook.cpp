b842ac7cf36bd0eb859d3aaf39494df7af55e574
manoda98 <manodasene@gmail.com>
2026-01-20 17:29:36 +0530

    implement sell side order insertion logic in OrderBook

--- matching-engine/src/OrderBook.cpp
+++ matching-engine/src/OrderBook.cpp
@@ -54,7 +54,52 @@ std::vector<Trade> OrderBook::addOrder(const Order& order) {
             orderPriceMap[workingOrder.orderId] = workingOrder.price;
         }
     } else {
-        
+        while (!workingOrder.isFilled() && !buyOrders.empty()) {
+            auto bestBidIt = buyOrders.begin();
+            double bestBidPrice = bestBidIt->first;
+
+            if (workingOrder.price > bestBidPrice) {
+                break;
+            }
+
+            auto& buyQueue = bestBidIt->second;
+            if (buyQueue.empty()) {
+                buyOrders.erase(bestBidIt);
+                continue;
+            }
+
+            Order& buyOrder = buyQueue.front();
+
+            double tradeQuantity = std::min(workingOrder.remainingSize, buyOrder.remainingSize);
+            double tradePrice = buyOrder.price;
+
+            Trade trade;
+            trade.tradeId = generateTradeId();
+            trade.buyOrderId = buyOrder.orderId;
+            trade.sellOrderId = workingOrder.orderId;
+            trade.symbol = workingOrder.symbol;
+            trade.price = tradePrice;
+            trade.quantity = tradeQuantity;
+            trade.timestamp = std::chrono::system_clock::now();
+            trades.push_back(trade);
+
+            workingOrder.remainingSize -= tradeQuantity;
+            buyOrder.remainingSize -= tradeQuantity;
+
+            if (buyOrder.isFilled()) {
+                orderPriceMap.erase(buyOrder.orderId);
+                buyQueue.pop();
+                if (buyQueue.empty()) {
+                    buyOrders.erase(bestBidIt);
+                }
+            }
+        }
+        if ( !workingOrder.isFilled()) {
+            sellOrders[workingOrder.price].push(workingOrder);
+            orderPriceMap[workingOrder.orderId] = workingOrder.price;
+        }
     }
+
+    return trades;
 
 }
